ac3de7dea145698fa34020c73e53a8dafd3f32de
omartag97 <omartageldin9797@gmail.com>
2025-10-07 13:29:24 +0300

    fix: update Logo link to redirect to the homepage

--- src/shared/components/app-layout/Logo.tsx
+++ src/shared/components/app-layout/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href="/movies" className="inline-block">
+    <Link href="/" className="inline-block">
       <h1 className="text-lg md:text-xl font-bold select-none">
         <span className="text-slate-900 dark:text-white">Nexus</span>
         <span className="bg-gradient-to-r from-blue-600 to-teal-400 bg-clip-text text-transparent dark:from-blue-400 dark:to-teal-300">
